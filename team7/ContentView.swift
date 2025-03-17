26820214d75504db698ab489af428d547a703f67
gatotbima1104 <gatotbima2002@gmail.com>
2025-03-17 11:44:55 +0700

    Initial Commit

---
+++ team7/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  team7
+//
+//  Created by Muhamad Gatot Supiadin on 17/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ team7/team7App.swift
@@ -0,0 +1,17 @@
+//
+//  team7App.swift
+//  team7
+//
+//  Created by Muhamad Gatot Supiadin on 17/03/25.
+//
+
+import SwiftUI
+
+@main
+struct team7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
